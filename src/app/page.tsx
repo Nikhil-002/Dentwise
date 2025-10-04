ac1813a2ed2f50b7ffbc74e7b2ebe3ace4c6923b
hello <nikhilk9350@gmail.com>
2025-10-04 17:13:58 +0530

    Added landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,21 @@
+import CTA from "@/components/landing/CTA";
+import Footer from "@/components/landing/Footer";
+import Header from "@/components/landing/Header";
+import Hero from "@/components/landing/Hero";
+import HowItWorks from "@/components/landing/HowItWorks";
+import PricingSection from "@/components/landing/PricingSection";
 import { SignedIn, SignOutButton } from "@clerk/nextjs";
 import { SignedOut, SignUpButton } from "@clerk/nextjs";
 
 export default function Home() {
   return (
-    <div>
-      <h1>Home Page</h1>
-      <SignedOut>
-        <SignUpButton mode="modal">SignUp</SignUpButton>      
-      </SignedOut>      
-
-      <SignedIn>
-        <SignOutButton>LogOut</SignOutButton>
-      </SignedIn>
+    <div className="min-h-screen bg-background">
+      <Header />
+      <Hero />
+      <HowItWorks />
+      <PricingSection />
+      <CTA />
+      <Footer />
     </div>
   );
 }
